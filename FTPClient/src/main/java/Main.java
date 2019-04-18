da256bf4de45a6b6d7bdc939c0c308177dab331b
Panda98 <810650217@qq.com>
2019-04-18 16:23:07 +0800

    添加list方法

--- FTPClient/src/main/java/Main.java
+++ FTPClient/src/main/java/Main.java
@@ -8,11 +8,12 @@ public class Main {
         FTPClient client = new FTPClient();
         String host = "192.168.2.7";
         int port = 21;
-        String username = "";
-        String password = "";
+        String username = "810650217@qq.com";
+        String password = "pyy19980118";
         try{
             client.connect(host,port,username,password);
-            client.upload("C:\\Users\\Pan\\Desktop\\hello.txt","test\\hello.txt");
+//            client.upload("C:\\Users\\Pan\\Desktop\\hello.txt","test\\hello.txt");
+            client.list("/");
         }catch (Exception e){
             e.printStackTrace();
         }finally {
